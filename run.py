4ff08dc52adc3906c64da3367b286f5b4d064467
KunalMalviya <kunalbhurajimalviya5@gmail.com>
2026-04-22 17:49:12 +0530

    do some change for deployment

--- run.py
+++ run.py
@@ -4,8 +4,10 @@ Or directly: uvicorn app.main:app --reload
 """
 import uvicorn
 from dotenv import load_dotenv
+import os
+
 
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", port= int(os.getenv("PORT")) , reload=True)
